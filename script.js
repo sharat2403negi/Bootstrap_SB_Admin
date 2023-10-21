d28c7e5e4aea2f24c844e7bf5ae8131c674bcb84
Sharat Negi <sharatgda@gmail.com>
2023-10-21 09:28:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+function vip() {
+    var element = document.getElementById("negi");
+    if (element.style.display === "none") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  }
+  
